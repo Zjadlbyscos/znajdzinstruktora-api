3a216f13dbef11319eaf3a52b4a53d825177a0a8
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-10-04 18:06:15 +0200

    Update getInstructors.js
    Updated getInstructors function to return paginated data

--- controllers/instructors/getInstructors.js
+++ controllers/instructors/getInstructors.js
@@ -2,8 +2,20 @@ import { Instructor } from "../../schemas/instructor.schema.js";
 
 export const getAllInstructors = async (req, res, next) => {
   try {
-    const allInstructors = await Instructor.find();
-    return res.status(200).json(allInstructors);
+    const { limit = 10, page = 1 } = req.query;
+    const pageLimit = Math.min(parseInt(limit), 50);
+    const skip = (parseInt(page) - 1) * pageLimit;
+
+    const instructors = await Instructor.find().skip(skip).limit(pageLimit);
+    const totalInstructors = await Instructor.countDocuments();
+    const totalPages = Math.ceil(totalInstructors / pageLimit);
+
+    return res.status(200).json({
+      instructors,
+      totalInstructors,
+      totalPages,
+      currentPage: parseInt(page),
+    });
   } catch (error) {
     next(error);
   }
