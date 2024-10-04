1e7e10dfe3084648475eb8e04ff3db11528d4150
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-10-04 18:27:07 +0200

    Update getAllFacilities.js
    Added pagination to getAllFacilities endpoint

--- controllers/object/getAllFacilities.js
+++ controllers/object/getAllFacilities.js
@@ -2,8 +2,20 @@ import { Facility } from "../../schemas/facility.schema.js";
 
 export const getAllFacilities = async (req, res, next) => {
   try {
-    const allFacilities = await Facility.find();
-    return res.status(200).json(allFacilities);
+    const { limit = 10, page = 1 } = req.query;
+    const pageLimit = Math.min(parseInt(limit), 50);
+    const skip = (parseInt(page) - 1) * pageLimit;
+
+    const allFacilities = await Facility.find().skip(skip).limit(pageLimit);
+    const totalFacilities = await Facility.countDocuments();
+    const totalPages = Math.ceil(totalFacilities / pageLimit);
+
+    return res.status(200).json({
+      facilities: allFacilities,
+      totalFacilities,
+      totalPages,
+      currentPage: parseInt(page),
+    });
   } catch (error) {
     next(error);
   }
