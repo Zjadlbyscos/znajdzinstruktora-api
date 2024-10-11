a78e5d6cd5baf04278b6420e06216db5002f7a4a
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-10-11 18:19:33 +0200

    Update getInstructors.js
    Implemented filter logic to getInstructors function

--- controllers/instructors/getInstructors.js
+++ controllers/instructors/getInstructors.js
@@ -2,13 +2,28 @@ import { Instructor } from "../../schemas/instructor.schema.js";
 
 export const getAllInstructors = async (req, res, next) => {
   try {
+    const { city, discipline, languages } = req.query;
+    const filters = {};
+
     const { limit = 10, page = 1 } = req.query;
     const pageLimit = Math.min(parseInt(limit), 50);
     const skip = (parseInt(page) - 1) * pageLimit;
 
-    const instructors = await Instructor.find().skip(skip).limit(pageLimit);
+    if (city) filters.city = city;
+    if (discipline) filters.discipline = discipline;
+    if (languages) filters.languages = languages;
+
+    Object.keys(filters).forEach(
+      (key) => filters[key] === undefined && delete filters[key]
+    );
+
+    const instructors = await Instructor.find(filters)
+      .skip(skip)
+      .limit(pageLimit);
     const totalInstructors = await Instructor.countDocuments();
     const totalPages = Math.ceil(totalInstructors / pageLimit);
+    console.log(req.query);
+    console.log(instructors);
 
     return res.status(200).json({
       instructors,
